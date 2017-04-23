5bc0a14f65222f0dc8b53e605cf4231ac3c41743
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2017-04-23 13:16:49 +0200

    Implement basic substituion

--- src/App.elm
+++ src/App.elm
@@ -1,7 +1,67 @@
 module App exposing (..)
 
-import Html exposing (Html, text, div, img)
+import Html exposing (Html, text, div, img, textarea)
 import Html.Attributes exposing (src)
+
+
+type Expression
+    = Name String
+    | Function String Expression
+    | Application Expression Expression
+
+
+type ParseError
+    = UnboundFunctionApplication String
+
+
+substitute : String -> Expression -> Expression -> Expression
+substitute from to exp =
+    case exp of
+        Name name ->
+            if name == from then
+                to
+            else
+                exp
+
+        Function arg body ->
+            if arg == from then
+                exp
+            else
+                Function arg <| substitute from to body
+
+        Application func arg ->
+            Application
+                (substitute from to func)
+                (substitute from to arg)
+
+
+apply : Expression -> Expression -> Result ParseError Expression
+apply func arg =
+    case func of
+        Name name ->
+            Err <| UnboundFunctionApplication name
+
+        Function name body ->
+            Ok <| substitute name arg body
+
+        Application _ _ ->
+            Result.andThen
+                (\f -> Ok (Application f arg))
+                (reduce func)
+
+
+reduce : Expression -> Result ParseError Expression
+reduce exp =
+    case exp of
+        Name name ->
+            Ok exp
+
+        Function arg body ->
+            Ok exp
+
+        Application func arg ->
+            apply func arg
+
 
 
 ---- MODEL ----
@@ -40,6 +100,7 @@ view model =
     div []
         [ img [ src model.logo ] []
         , div [] [ text model.message ]
+        , textarea [] []
         ]
 
 
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -3,7 +3,7 @@ module Tests exposing (..)
 import Test exposing (..)
 import Expect
 import String
-import App
+import App exposing (..)
 
 
 all : Test
@@ -11,14 +11,53 @@ all =
     describe "A Test Suite"
         [ test "App.model.message should be set properly" <|
             \() ->
-                Expect.equal (Tuple.first (App.init  "../src/logo.svg") |> .message) "Your Elm App is working!"
-        , test "Addition" <|
+                Expect.equal (Tuple.first (App.init "../src/logo.svg") |> .message) "Your Elm App is working!"
+        , test "root name is not reduced" <|
             \() ->
-                Expect.equal (3 + 7) 10
-        , test "String.left" <|
+                Expect.equal
+                    (reduce <| Name "a")
+                    (Ok <| Name "a")
+        , test "root function is not reduced" <|
             \() ->
-                Expect.equal "a" (String.left 1 "abcdefg")
-        , test "This test should fail" <|
+                Expect.equal
+                    (reduce <|
+                        Function "foo" (Name "foo")
+                    )
+                    (Ok <| Function "foo" (Name "foo"))
+        , test "reduce identity application" <|
             \() ->
-                Expect.fail "failed as expected!"
+                Expect.equal
+                    (reduce <|
+                        Application
+                            (Function "a" <| Name "a")
+                            (Name "hello")
+                    )
+                    (Ok <| Name "hello")
+        , test "reduce constant application" <|
+            \() ->
+                Expect.equal
+                    (reduce <|
+                        Application
+                            (Function "_" <| Name "a")
+                            (Name "hello")
+                    )
+                    (Ok <| Name "a")
+        , test "reduce identity application" <|
+            \() ->
+                Expect.equal
+                    (reduce <|
+                        Application
+                            (Function "x" <| Name "x")
+                            (Name "hello")
+                    )
+                    (Ok <| Name "hello")
+        , test "application on unbound function" <|
+            \() ->
+                Expect.equal
+                    (reduce <|
+                        Application
+                            (Name "foo")
+                            (Name "bar")
+                    )
+                    (Err <| UnboundFunctionApplication "foo")
         ]
